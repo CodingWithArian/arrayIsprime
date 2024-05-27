020e8eaf5c97229255e47d2660c7408cb177bccc
arian <arian@DESKTOP-7RE7FHT>
2024-05-27 20:47:50 +0400

    Add project files.

---
+++ arrayIsprime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayIsprime
+{
+    internal class Program
+    {
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= n / 2; i++)
+            
+                if (n % i == 0) return false;
+                return true;
+            
+        }
+        static void Main(string[] args)
+        {
+            int count = 0;string output="",primeNums = "";
+            int[,] numbers = new int[2,2];
+            Console.WriteLine("Enter Array's elements");
+            for(int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.WriteLine("row{0}", i);
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                    numbers[i, j] = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("Your Array is");
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    output += numbers[i, j] + "\t";
+                    if (IsPrime(numbers[i,j]))
+                    {
+                        primeNums += numbers[i,j] + "\t";
+                    }
+                }
+
+                output += "\n";
+            }
+            Console.WriteLine(output);
+            Console.WriteLine("your prime numbers are {0}", primeNums);
+
+            Console.ReadKey();
+
+        }
+    }
+}
